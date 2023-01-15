781de3f981db41517dd888c5d8fcfd7ec622ee01
Spencer Kinsey-Korzym <34325816+huhu72@users.noreply.github.com>
2023-01-15 17:29:02 -0500

    fixed cors error

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const express = require('express')
-const cors = require('cors')
-const rateLimit = require('express-rate-limit')
+const express = require("express");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
 //Rate Limiting
 const limiter = rateLimit({
-    windowMs: 2*1000,
-    max: 20
-})
+  windowMs: 2 * 1000,
+  max: 20,
+});
 
-app.use(limiter)
-app.set('trust proxy, 1')
-
-app.use('/summoner', require('./routes/riot'))
+app.use(limiter);
+app.set("trust proxy, 1");
 //Enable cors
-app.use(cors())
+app.use(cors());
 
-app.listen(PORT, () => console.log('workin'))
+app.use("/summoner", require("./routes/riot"));
+
+app.set;
+app.listen(PORT, () => console.log("workin"));
