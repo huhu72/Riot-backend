50691fe29de21539686ecb6f4a3aac264e0daa93
Spencer Kinsey-Korzym <34325816+huhu72@users.noreply.github.com>
2023-01-15 17:34:07 -0500

    fixed ranked endpoint

--- routes/riot.js
+++ routes/riot.js
@@ -8,17 +8,19 @@ const url = require("url");
 const API_BASE_URL = "https://na1.api.riotgames.com";
 const API_KEY = process.env.API_KEY;
 const API_SUMMONER_URL = "/lol/summoner/v4/summoners/by-name/";
-const API_RANKED_URL = "/lol/league/v4/entries/by-summoner/"
-const summonerID = ""
+const API_RANKED_URL = "/lol/league/v4/entries/by-summoner/";
+const summonerID = "";
 const params = new URLSearchParams({
-    api_key: API_KEY,
-  });
+  api_key: API_KEY,
+});
 
 router.get("/", async (req, res) => {
   try {
-
     const requestData = url.parse(req.url, true).query.summoner;
-    const apiRes = await needle("get", `${API_BASE_URL}${API_SUMMONER_URL}${requestData}?${params}`);
+    const apiRes = await needle(
+      "get",
+      `${API_BASE_URL}${API_SUMMONER_URL}${requestData}?${params}`
+    );
     const data = apiRes.body;
     res.status(200).json(data);
   } catch (error) {
@@ -26,26 +28,31 @@ router.get("/", async (req, res) => {
   }
 });
 router.get("/ranked", async (req, res) => {
-    try {
-      const requestData = url.parse(req.url, true).query.summoner;
-      const apiRes = await needle("get", `${API_BASE_URL}${API_SUMMONER_URL}${requestData}?${params}`);
-      this.summonerID= apiRes.body.id;
-      res.redirect(`ranked/info?summoner=${requestData}`)
-    } catch (error) {
-      res.status(500).json("error: in redirect");
-    }
-  });
+  try {
+    const requestData = url.parse(req.url, true).query.summoner;
+    const apiRes = await needle(
+      "get",
+      `${API_BASE_URL}${API_SUMMONER_URL}${requestData}?${params}`
+    );
+    this.summonerID = apiRes.body.id;
+    res.redirect(`ranked/info?summoner=${requestData}`);
+  } catch (error) {
+    res.status(500).json("error: in redirect");
+  }
+});
 
-  router.get('/ranked/info', async(req, res) =>{
-    console.log("redirected")
-    try {
-        const apiRes = await needle("get", `${API_BASE_URL}${API_RANKED_URL}${this.summonerID}?${params}`);
-        const data = apiRes.body;
-        res.status(200).json(data)
-    } catch (error) {
-        res.status(500).json("error");
-    }
-  })
-
+router.get("/ranked/info", async (req, res) => {
+  console.log("redirected");
+  try {
+    const apiRes = await needle(
+      "get",
+      `${API_BASE_URL}${API_RANKED_URL}${summonerID}?${params}`
+    );
+    const data = apiRes.body;
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json("error");
+  }
+});
 
 module.exports = router;
