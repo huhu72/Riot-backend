ac52ca9d8e43debcf58bc3e0f69887defb0c6013
Spencer Kinsey-Korzym <34325816+huhu72@users.noreply.github.com>
2023-05-08 00:25:27 -0400

    debugging

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const rateLimit = require("express-rate-limit");
+const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,16 +10,16 @@ const app = express();
 
 //Rate Limiting
 const limiter = rateLimit({
-  windowMs: 1 * 1000,
-  max: 15,
+	windowMs: 1 * 1000,
+	max: 15,
 });
 
 app.use(limiter);
-app.set("trust proxy, 1");
+app.set('trust proxy, 1');
 //Enable cors
 app.use(cors());
 
-app.use("/summoner", require("./routes/riot"));
+app.use('/summoner', require('./routes/riot'));
 
 app.set;
-app.listen(PORT, () => console.log("workin"));
+app.listen(PORT, () => console.log(`workin on ${PORT}`));
